b3cbcda1863a99cf7ff24998eaf223b5365d9676
Arati somwanshi <7arati10@gmail.com>
2025-02-04 12:17:39 +0530

    Update AccountsController.cs

--- Bank Management System/WebApplication10/Controllers/AccountsController.cs
+++ Bank Management System/WebApplication10/Controllers/AccountsController.cs
@@ -48,7 +48,7 @@ namespace WebApplication10.Controllers
 
         // POST: Accounts/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("AccountId,UserId,AccountNumber,Balance")] Account account)
